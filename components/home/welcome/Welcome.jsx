89f096f8d9217ae5457e9a42d3bef5c04a34cd54
Tam Nguyen <tamnmdn@gmail.com>
2023-03-30 16:53:48 +0700

    init commit

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+
+import { icons, SIZES } from "../../../constants";
+import styles from "./welcome.style";
+import { useRouter } from "expo-router";
+
+const Welcome = () => {
+  const router = useRouter();
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
